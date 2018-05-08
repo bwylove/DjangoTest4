ebd4b888b2d67aee42bd49dd602d6e52c11117f9
bwylove <bwylove@126.com>
2018-05-08 17:44:31 +0800

    模板

--- booktest/urls.py
+++ booktest/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     url(r'^user1$', views.user1, name='user1'),
     url(r'^user2$', views.user2, name='user2'),
     url(r'htmlTest$',views.htmlTest),
-    url(r'^csrf1',views.csrf1),
-    url(r'^csrf2',views.csrf2),
-
+    url(r'^csrf1$',views.csrf1),
+    url(r'^csrf2$',views.csrf2),
+    url(r'^verifyCode$',views.verifyCode),
+    url(r'^verifyTest1$',views.verifyTest1),
+    url(r'^verifyTest2$',views.verifyTest2),
 ]
