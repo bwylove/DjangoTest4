1f1499b1c7bb7b6a6595c515765d092627982974
bwylove <bwylove@126.com>
2018-05-08 10:04:11 +0800

    模板

--- booktest/views.py
+++ booktest/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from models import *
 # Create your views here.
 def index(request):
-    hero=HeroInfo.objects.get(pk=1)
-    context ={'hero':hero}
+    # hero=HeroInfo.objects.get(pk=1)
+    # context ={'hero':hero}
+    list=HeroInfo.objects.filter(isDetele=False)
+    context={"list":list}
     return render(request,'booktest/index.html',context)
